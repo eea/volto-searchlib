b9419f231af40bc1826cb74c6f1fb995df311b89
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2023-03-13 13:14:16 +0200

    use registry.resolve for healthcheck

--- src/middleware/healthcheck.js
+++ src/middleware/healthcheck.js
@@ -3,9 +3,9 @@ import { registry } from '@eeacms/search';
 export default function healthcheck(req, res, next) {
   const { id } = req.params;
   const appConfig = registry.searchui[id];
-  if (appConfig?.healthcheck) {
-    appConfig
-      .healthcheck(appConfig)
+  const hc = registry.resolve[appConfig?.healthcheck];
+  if (hc) {
+    hc(appConfig)
       .then((body) => res.send(body))
       .catch((body) => {
         res.send({ error: body });
