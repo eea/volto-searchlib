3bcedd45fdc65b258fbbfc5a580f5a8b8a9b6853
Tiberiu Ichim <tiberiuichim@users.noreply.github.com>
2023-05-29 14:41:52 +0300

    fix: don't use the default sort when loading a page with a search term - refs #253324

--- searchlib/components/SearchApp/SearchApp.jsx
+++ searchlib/components/SearchApp/SearchApp.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { SearchView } from '@eeacms/search/components/SearchView/SearchView';
 import { getDefaultFilters } from '@eeacms/search/lib/utils';
+import qs from 'querystring';
 
 import BasicSearchApp from './BasicSearchApp';
 
 export default function SearchApp(props) {
-  const { defaultSort = '' } = props;
+  const locationSearch = __CLIENT__
+    ? qs.parse(window.location.search || '')?.['q']
+    : null;
+  const defaultSort = locationSearch !== null ? '' : props.defaultSort || '';
   const [sortField, sortDirection] = defaultSort.split('|');
   const appConfig = props.registry.searchui[props.appName];
   const appDefaultFilters = getDefaultFilters(appConfig);
