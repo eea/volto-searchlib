160c24ab7e554007f51218e06ce12a343a2d4368
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2023-07-25 15:22:45 +0300

    moved getInfo to lib/getIndexInfo.js

--- searchlib/lib/getIndexInfo.js
+++ searchlib/lib/getIndexInfo.js
@@ -1,10 +1,11 @@
 import superagent from 'superagent';
+import { DateTime } from 'luxon';
 
 function trim_slash(text) {
   return text.replace(/^\/+|\/+$/g, '');
 }
 
-export default async function getIndexInfo(config) {
+async function getIndexInfo(config) {
   const { host, elastic_index } = config;
   const settings_url =
     trim_slash(host) + '/' + trim_slash(elastic_index) + '/_settings';
@@ -21,5 +22,39 @@ export default async function getIndexInfo(config) {
     return { settings: settings_resp.body || {}, alias: alias_resp.body || {} };
   } catch (e) {
     return { error: true, statusCode: 500, body: `An error occurred: ${e}` };
+  }
+}
+
+export default async function getInfo(appConfig) {
+  const { elastic_index } = appConfig;
+
+  if (elastic_index === '_all') return ''; // we don't support _all
+
+  const info = await getIndexInfo(appConfig);
+
+  if (info.error || info.detail) {
+    // eslint-disable-next-line
+    console.warn('Error in retrieving index info', info);
+    return '';
+  }
+
+  try {
+    const indexes = Object.keys(info['settings']);
+    if (indexes.length < 1) return '';
+    let aliases = Object.keys(info.alias[indexes[0]]['aliases']);
+    aliases = aliases
+      .filter((alias) => alias.startsWith('updated_at_'))
+      .sort()
+      .reverse();
+    let update_ts = info.settings[indexes[0]].settings.index.creation_date;
+    if (aliases.length > 0) {
+      update_ts = aliases[0].substring(11);
+    }
+
+    const dt = DateTime.fromMillis(parseInt(update_ts));
+    return dt;
+  } catch {
+    console.log('info', info);
+    return '';
   }
 }
